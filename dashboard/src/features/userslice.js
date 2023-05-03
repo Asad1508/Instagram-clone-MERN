36b3c82776c70edb5fe02fbea17a8087e21ae229
Asad1508 <109195461+Asad1508@users.noreply.github.com>
2023-05-03 12:14:17 +0500

    Update userslice.js

--- dashboard/src/features/userslice.js
+++ dashboard/src/features/userslice.js
@@ -9,13 +9,11 @@ export const userSlice=createSlice({
     name:'user_info',
     initialState,
     reducers:{
-        //setuserinfo jaha call kre gay waha se ham pass kre gay email aur name
-        //aur uske bd set krde gay neechy waly step me name aur email
+       
         setUserInfo:(state,action)=>{
             state.email=action.payload.email
             state.name=action.payload.name
         },
-        //isme empty data pass kr k unset kr skty
         unsetUserInfo:(state,action)=>{
             state.email=action.payload.email
             state.name=action.payload.name
@@ -24,6 +22,3 @@ export const userSlice=createSlice({
 })
 export const {setUserInfo,unsetUserInfo}=userSlice.actions
 export default userSlice.reducer
-
-//sab krne k bd iss slice ko store.js me mention kro
-//redux ko configure krne k liye slice bnatay aur bd me store me daltay
