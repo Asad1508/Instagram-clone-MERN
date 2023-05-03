1ec6920982d99bc16a2a8e3ff388ffb51df925e0
Asad1508 <109195461+Asad1508@users.noreply.github.com>
2023-05-03 12:13:53 +0500

    Update routeauthslice.js

--- dashboard/src/features/routeauthslice.js
+++ dashboard/src/features/routeauthslice.js
@@ -1,5 +1,4 @@
-//isme code kia takay user agar login ha tu dashboard me reh kr b url me login type kr k 
-// dashboard me he rhe
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
@@ -10,13 +9,10 @@ export const routeauthSlice=createSlice({
     name:'auth_token',
     initialState,
     reducers:{
-        //setuserinfo jaha call kre gay waha se ham pass kre gay email aur name
-        //aur uske bd set krde gay neechy waly step me name aur email
         setUsertoken:(state,action)=>{
             state.token=action.payload.token
             
         },
-        //isme empty data pass kr k unset kr skty
         unsetUsertoken:(state,action)=>{
             state.token=action.payload.token
             
@@ -25,6 +21,3 @@ export const routeauthSlice=createSlice({
 })
 export const {setUsertoken,unsetUsertoken}=routeauthSlice.actions
 export default routeauthSlice.reducer
-
-//sab krne k bd iss slice ko store.js me mention kro
-//redux ko configure krne k liye slice bnatay aur bd me store me daltay
